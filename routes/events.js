a71f26feec86a5eae1cbc4efa87b9571faedc410
André de Albuquerque <itstheandre@gmail.com>
2021-03-18 20:36:43 +0100

    Posted to github

--- routes/events.js
+++ routes/events.js
@@ -4,8 +4,24 @@ const Event = require("../models/Event.model");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  Event.find().then((alLEvents) => {
+  const eventName = new RegExp(req.query.event, "i");
+  Event.find({ name: { $regex: eventName } }).then((alLEvents) => {
     console.log("alLEvents:", alLEvents);
+    res.render("eventPage", { eventList: alLEvents });
+  });
+});
+
+router.get("/all", (req, res) => {
+  Event.find({}).then((allEvents) => {
+    res.render("eventPage", { eventList: allEvents });
+  });
+});
+
+// /events/*
+router.get("/:dynamicGorilla", (req, res) => {
+  Event.findOne({ slug: req.params.dynamicGorilla }).then((thatSingleEvent) => {
+    console.log("thatSingleEvent:", thatSingleEvent);
+    res.render("single-event", { event: thatSingleEvent });
   });
 });
 
